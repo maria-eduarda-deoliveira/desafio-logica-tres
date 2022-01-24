ec14b540b5723f8c34ab9375117601c4a63742a1
Maria Eduarda de Oliveira Garcia <ol.mariae@outlook.com>
2022-01-23 21:55:43 -0300

    desafio tres

---
+++ desafio-tres.js
@@ -0,0 +1,16 @@
+for(let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+let i = 0;
+while (i < 10) {
+i++;
+  console.log(i)
+}
+
+for(let i = 1; i <= 100; i += 2){
+    console.log(i);
+}
+for(let i = 0; i <= 100; i += 2){
+    console.log(i);
+}
